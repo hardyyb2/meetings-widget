8a35248555f5c3db319855648e4b70985a3b2b53
hardik badola <hardikb3732@gmail.com>
2023-08-28 16:35:06 +0530

    [UPDATED] form

--- src/App.tsx
+++ src/App.tsx
@@ -36,43 +36,37 @@ function App() {
             required
           />
         </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Company (optional)</span>
-          </label>
-          <input
-            name="company"
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-        <div className="form-control mt-4">
+
+        <div className="label mt-2">Company size (optional)</div>
+        <div className="form-control">
           <label className="label cursor-pointer">
-            <span className="label-text">Backend engineer</span>
+            <span className="label-text">1 to 50</span>
             <input
+              value="1 to 50"
               type="radio"
-              name="profession"
+              name="companySize"
               className="radio checked:bg-red-500"
             />
           </label>
         </div>
         <div className="form-control">
           <label className="label cursor-pointer">
-            <span className="label-text">Frontend engineer</span>
+            <span className="label-text">51 to 250</span>
             <input
+              value="51 to 250"
               type="radio"
-              name="profession"
+              name="companySize"
               className="radio checked:bg-blue-500"
             />
           </label>
         </div>
         <div className="form-control">
           <label className="label cursor-pointer">
-            <span className="label-text">Fullstack engineer</span>
+            <span className="label-text">251 to 500</span>
             <input
+              value="251 to 500"
               type="radio"
-              name="profession"
+              name="companySize"
               className="radio checked:bg-blue-500"
             />
           </label>
